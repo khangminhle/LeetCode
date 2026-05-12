9cd0c76f3b23f5e4d0a3452332195d5aaafe146c
Khang Minh Le <15520339@gm.uit.edu.vn>
2026-05-12 15:52:04 +0700

    Update container-with-most-water.cpp

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -8,14 +8,16 @@ public:
             
             int min = -1;
             int area = right-left;
-                                                            if(height[right]>=height[left]){
-                                                                min = height[left];                                                                                            left+=1;
-} else {
-    min = height[right];
-    right-=1;}
-    area = area*min;
-    if(area > maxarea){maxarea=area;}
-        }     
+            if(height[right]>=height[left]){
+                min = height[left];
+                left+=1;
+            } else {
+                min = height[right];
+                right-=1;
+            }
+                area = area*min;
+                if(area > maxarea){maxarea=area;}
+        }
         return maxarea;
     }
 };
