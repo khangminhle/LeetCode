370819dc4ea799429cbb33b798ac9644f1ce76af
Khang Minh Le <15520339@gm.uit.edu.vn>
2026-05-14 11:13:57 +0700

    Time: 0 ms (100.00%) | Memory: 26.3 MB (49.31%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -11,7 +11,7 @@ public:
         int duplicate = -1;
         int errorNumber = -1;
 
-        for(int i = 1; i <= n+1; i++) {
+        for(int i = 1; i <= n; i++) {
             if(hash[i] == 2) {
                 duplicate = i;
             } else {
