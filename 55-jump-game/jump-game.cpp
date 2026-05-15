def4b325c5d6ba3dcd7f1578f5431fcc4b492e51
Khang Minh Le <15520339@gm.uit.edu.vn>
2026-05-15 10:53:46 +0700

    Time: 0 ms (100.00%) | Memory: 52.3 MB (81.56%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -2,30 +2,27 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
+    
+        int maxJump = 0;
         int goal = n-1;
-        int i = goal-1;
-        while(i >= 0) {
-            if(i + nums[i] >= goal) {
-                goal = i;
-            } else {
-                int j = i-1;
-                if(j < 0) {return false;}
-                bool flag = false;
-                while(j >= 0) {
-                    if(j + nums[j] >= goal) {
-                        flag = true;
-                        break;
-                    }
-                    j--;
-                }
 
-                if(flag == false) {
-                    return false;
-                }
+        for(int i = 0; i < n; i++) {
 
+            if(maxJump < i) {
+                return false;
             }
-            i--;
+
+            int ableToJump = i + nums[i];
+            
+            if(maxJump < ableToJump) {
+                maxJump = ableToJump;
+            }
+
+            if(maxJump >= n-1) {
+                return true;
+            }
         }
+
         return true;
     }
 };
