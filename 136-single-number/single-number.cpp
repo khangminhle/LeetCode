36a4f85f39cedd21e67a0c5bc7b842752ebb7db5
Khang Minh Le <15520339@gm.uit.edu.vn>
2026-05-24 19:45:46 +0700

    Time: 0 ms (100.00%) | Memory: 20.8 MB (14.11%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int, int> hm;
+        int ans = 0;
         int n = nums.size();
+
         for(int i = 0; i < n; i++) {
-            if(hm.count(nums[i]) > 0) {
-                hm[nums[i]] += 1;
-            } else {
-                hm[nums[i]] = 1;
-            }
+            ans = ans ^ nums[i];
         }
 
-        for (pair<int, int> it: hm) {
-            //cout<<it.first<<endl;
-            if(it.second == 1) {
-                return it.first;
-            }
-        }
-
-        return 0;
-
+        return ans;
     }
 };
